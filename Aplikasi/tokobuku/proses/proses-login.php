9f08eeeb781aeb929f11b3ce8a40e8d1bf3f64ee
ferdy42id <pinoan42@gmail.com>
2020-08-17 17:03:32 +0800

    proses login use new structure

--- Aplikasi/tokobuku/proses/proses-login.php
+++ Aplikasi/tokobuku/proses/proses-login.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-if(isset($_POST['submit'])){
-	include "Kasir.php";
-	$Kasir = new Kasir;
-	$Kasir->setUsername(mysql_real_escape_string($_POST['username']));
-	$Kasir->setPassword(mysql_real_escape_string($_POST['password']));
-	$Kasir->login();
+if (isset($_POST['submit'])) {
+    include "Kasir.php";
+    $Kasir    = new Kasir();
+    $mysqli  = $Kasir->connection()->mysqli();
+    $Kasir->__set('username', $mysqli->real_escape_string($_POST['username']));
+    $Kasir->__set('password', $mysqli->real_escape_string($_POST['password']));
+    $Kasir->login();
 }
-?>
